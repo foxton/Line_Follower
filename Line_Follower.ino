fed023c16174aa79463ab0df2ec2a71f1f8a1ece
foxton <foxx89@hotmail.com>
2014-03-19 23:11:38 +0100

    V0.1

--- Line_Follower.ino
+++ Line_Follower.ino
@@ -1,50 +1,36 @@
-double sensorPin = A0;  //TCRT5000 sensor connected to A0
-double sensorPin2 = A1;  //TCRT5000 sensor connected to A0
-double sensorPin3 = A2;  //TCRT5000 sensor connected to A0
-double sensorState = 0;
-double sensorState2 = 0;
-double sensorState3 = 0;
-double average = 0;
-double average2 = 0;
-double average3 = 0;
+	double sensorLeft = A0; //TCRT5000 sensor connected to A0
+    double sensorCenter = A1; //TCRT5000 sensor connected to A1
+    double sensorRight = A2; //TCRT5000 sensor connected to A2
+    double sensorStateLeft = 0;
+    double sensorStateCenter = 0;
+    double sensorStateRight = 0;
+    double average = 0;
+    String Tab = "\t";
+    String NewLine = "\n";
+    void setup()
+    {
+    pinMode(sensorLeft,INPUT);
+    pinMode(sensorCenter,INPUT);
+    pinMode(sensorRight,INPUT);
+    Serial.begin(9600);
+    }
+    void loop()
+    {
+    sensorStateLeft = analogReadAverage(sensorLeft, 10);
+    sensorStateCenter = analogReadAverage(sensorCenter, 10);
+    sensorStateRight = analogReadAverage(sensorRight, 10);
+    Serial.print(sensorStateLeft + Tab );
+    Serial.print(sensorStateCenter + Tab );
+    Serial.print(sensorStateRight + Tab + NewLine);
+    delay(250);
+    }
+    double analogReadAverage(double SensorPosition, int samples)
+    {
+    double value = 0;
+    for (int i = 0; i < samples; i++)
+    {
+    value += analogRead(SensorPosition);
+    }
+    return value / samples;
+    }
 
-void setup()
-{
- pinMode(sensorPin,INPUT); 
-  pinMode(sensorPin2,INPUT); 
-   pinMode(sensorPin3,INPUT); 
- Serial.begin(9600);
-}
-void loop()
-{
-	average = 0;
-	average2 = 0;
-	average3 = 0;
-
-  for(int i = 0; i < 10; i++)    //Take 10 samples
-  { 
-   average += analogRead(sensorPin);
- }
-
-  for(int i = 0; i < 10; i++)    //Take 10 samples
-  { 
-   average2 += analogRead(sensorPin2);
- }
-
-  for(int i = 0; i < 10; i++)    //Take 10 samples
-  { 
-   average3 += analogRead(sensorPin3);
- }
- 
- sensorState = average  / 10;    //Average of samples
-  sensorState2 = average2  / 10; 
-   sensorState3 = average3  / 10; 
-  Serial.print(sensorState);
-  Serial.print("\t");
-  Serial.print(sensorState2);
-  Serial.print("\t");
-  Serial.print(sensorState3);
-  Serial.print("\t");
-  Serial.print("\n");
-  delay(250);
-}
