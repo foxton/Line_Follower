ce04f6b652c819c6cfa65ae80105c696f3126bb5
foxton <foxx89@hotmail.com>
2014-03-19 22:08:13 +0100

    Line Follower

--- Line_Follower.ino
+++ Line_Follower.ino
@@ -1,48 +1,50 @@
-double sensorPin1 = A0;  //3 TCRT5000 sensors connected to A0-A2
-double sensorPin2 = A1;
-double sensorPin3 = A2;
-int ledPin1 = 3;         //IR diode (TCRT5000) connected to pin 3
-int ledPin2 = 5;         //IR diode (TCRT5000) connected to pin 5
-int ledPin3 = 6;         //IR diode (TCRT5000) connected to pin 6
+double sensorPin = A0;  //TCRT5000 sensor connected to A0
+double sensorPin2 = A1;  //TCRT5000 sensor connected to A0
+double sensorPin3 = A2;  //TCRT5000 sensor connected to A0
 double sensorState = 0;
+double sensorState2 = 0;
+double sensorState3 = 0;
 double average = 0;
+double average2 = 0;
+double average3 = 0;
 
 void setup()
 {
- pinMode(sensorPin1,INPUT); 
- pinMode(sensorPin2,INPUT); 
- pinMode(sensorPin3,INPUT); 
- pinMode(ledPin1,OUTPUT);
- pinMode(ledPin2,OUTPUT);
- pinMode(ledPin3,OUTPUT);
+ pinMode(sensorPin,INPUT); 
+  pinMode(sensorPin2,INPUT); 
+   pinMode(sensorPin3,INPUT); 
  Serial.begin(9600);
 }
 void loop()
 {
-  average = 0;
+	average = 0;
+	average2 = 0;
+	average3 = 0;
 
-digitalWrite(ledPin1,HIGH);  //Start IR LED
-digitalWrite(ledPin2,HIGH);  //Start IR LED
-digitalWrite(ledPin3,HIGH);  //Start IR LED
   for(int i = 0; i < 10; i++)    //Take 10 samples
   { 
-   average += analogRead(sensorPin1);
-  }
+   average += analogRead(sensorPin);
+ }
+
+  for(int i = 0; i < 10; i++)    //Take 10 samples
+  { 
+   average2 += analogRead(sensorPin2);
+ }
+
+  for(int i = 0; i < 10; i++)    //Take 10 samples
+  { 
+   average3 += analogRead(sensorPin3);
+ }
  
-  sensorState = average / 10;    //Average of samples
+ sensorState = average  / 10;    //Average of samples
+  sensorState2 = average2  / 10; 
+   sensorState3 = average3  / 10; 
   Serial.print(sensorState);
+  Serial.print("\t");
+  Serial.print(sensorState2);
+  Serial.print("\t");
+  Serial.print(sensorState3);
+  Serial.print("\t");
   Serial.print("\n");
-  delay(100);
-
-  if(sensorState > 800)  // > 800 = black line
-  {
-
-  }
-  else					// < 800 = white line
-  {
-
-  }
-
-
+  delay(250);
 }
-
